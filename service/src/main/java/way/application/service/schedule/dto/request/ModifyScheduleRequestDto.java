bec982a5c5b4b5d95c885b0d54a2824ff64a5253
euics <pjh575157@gmail.com>
2024-07-10 18:30:08 +0900

    feat: ModifyScheduleRequest와 ModifyScheduleResponse 레코드 및 DTO 변환 추가 (#9)
    - `way.presentation.schedule.vo.req` 패키지에 `ModifyScheduleRequest` 레코드 생성
      - 필드: scheduleSeq, title, startTime, endTime, location, streetName, x, y, color, memo, invitedMemberSeqs, createMemberSeq
      - `ModifyScheduleRequestDto`로 변환하는 메서드 추가
    
    - `way.presentation.schedule.vo.res` 패키지에 `ModifyScheduleResponse` 레코드 생성
      - 필드: scheduleSeq
    
    - `way.application.service.schedule.dto.request` 패키지에 `ModifyScheduleRequestDto` 레코드 생성
      - 필드: scheduleSeq, title, startTime, endTime, location, streetName, x, y, color, memo, invitedMemberSeqs, createMemberSeq
      - `SaveScheduleRequestDto`로 변환하는 메서드 추가
    
    - `way.application.service.schedule.dto.response` 패키지에 `ModifyScheduleResponseDto` 레코드 생성
      - 필드: scheduleSeq

---
+++ service/src/main/java/way/application/service/schedule/dto/request/ModifyScheduleRequestDto.java
@@ -0,0 +1,46 @@
+package way.application.service.schedule.dto.request;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record ModifyScheduleRequestDto(
+	Long scheduleSeq,
+
+	String title,
+
+	LocalDateTime startTime,
+
+	LocalDateTime endTime,
+
+	String location,
+
+	String streetName,
+
+	Double x,
+
+	Double y,
+
+	String color,
+
+	String memo,
+
+	List<Long> invitedMemberSeqs,
+
+	Long createMemberSeq
+) {
+	public SaveScheduleRequestDto toSaveScheduleRequestDto() {
+		return new SaveScheduleRequestDto(
+			this.title,
+			this.startTime,
+			this.endTime,
+			this.location,
+			this.streetName,
+			this.x,
+			this.y,
+			this.color,
+			this.memo,
+			this.invitedMemberSeqs,
+			this.createMemberSeq
+		);
+	}
+}
