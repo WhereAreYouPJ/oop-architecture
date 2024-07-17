d74d3c7c3a86754d135b21e7fe872e67fa20d9bb
euics <pjh575157@gmail.com>
2024-07-17 16:38:25 +0900

    feat: FeedImageMapper, FeedMapper, FeedMemberMapper 인터페이스 추가 (#26)
    - FeedImageMapper 인터페이스를 생성하고 toFeedImageEntity 메서드를 정의하였습니다.
      - feedImageSeq 필드를 무시하고, feed 및 feedImageURL 필드를 매핑하도록 설정.
      - @Mapper 어노테이션을 사용하여 스프링 컴포넌트로 등록하고 생성자 주입 및 미매핑 대상 정책을 설정.
    
    - FeedMapper 인터페이스를 생성하고 toFeedEntity 메서드를 정의하였습니다.
      - feedSeq 필드를 무시하고, schedule, creatorMember, title, content 필드를 매핑하도록 설정.
      - @Mapper 어노테이션을 사용하여 스프링 컴포넌트로 등록하고 생성자 주입 및 미매핑 대상 정책을 설정.
    
    - FeedMemberMapper 인터페이스를 생성하고 toFeedMemberEntity 메서드를 정의하였습니다.
      - feedMemberSeq 필드를 무시하고, FeedEntity 및 MemberEntity를 매핑하도록 설정.
      - @Mapper 어노테이션을 사용하여 스프링 컴포넌트로 등록하고 생성자 주입 및 미매핑 대상 정책을 설정.

--- service/src/main/java/way/application/service/feedImage/mapper/FeedImageMapper.java
+++ service/src/main/java/way/application/service/feedImage/mapper/FeedImageMapper.java
@@ -5,10 +5,13 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
-import way.application.infrastructure.schedule.entity.ScheduleEntity;
-import way.application.service.schedule.dto.request.ScheduleRequestDto;
+import way.application.infrastructure.feed.entity.FeedEntity;
+import way.application.infrastructure.feedImage.entity.FeedImageEntity;
 
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface FeedImageMapper {
-
+	@Mapping(target = "feedImageSeq", ignore = true)
+	@Mapping(target = "feed", source = "feed")
+	@Mapping(target = "feedImageURL", source = "feedImageURL")
+	FeedImageEntity toFeedImageEntity(FeedEntity feed, String feedImageURL);
 }
