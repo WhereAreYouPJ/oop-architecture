3cdb2b4da219ee3e00bebb9da3ef47b45593fefc
euics <pjh575157@gmail.com>
2024-07-17 16:39:09 +0900

    feat: FeedRequestVo 및 FeedResponseVo 클래스 추가 (#26)
    - FeedRequestVo 클래스 내에 SaveFeedRequest 레코드를 생성하고, Feed 요청 데이터를 캡슐화.
      - scheduleSeq, creatorSeq, title, content, images 필드를 포함.
      - toSaveFeedRequest 메서드를 통해 SaveFeedRequestDto 객체로 변환 가능.
    
    - FeedResponseVo 클래스 내에 SaveFeedResponse 레코드를 생성하고, Feed 응답 데이터를 캡슐화.
      - feedSeq 필드를 포함.

---
+++ presentation/src/main/java/way/presentation/feed/vo/req/FeedRequestVo.java
@@ -0,0 +1,27 @@
+package way.presentation.feed.vo.req;
+
+import static way.application.service.feed.dto.request.FeedRequestDto.*;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class FeedRequestVo {
+	public record SaveFeedRequest(
+		Long scheduleSeq,
+		Long creatorSeq,
+		String title,
+		String content,
+		List<MultipartFile> images
+	) {
+		public SaveFeedRequestDto toSaveFeedRequest() {
+			return new SaveFeedRequestDto(
+				this.scheduleSeq,
+				this.creatorSeq,
+				this.title,
+				this.content,
+				this.images
+			);
+		}
+	}
+}
---
+++ presentation/src/main/java/way/presentation/feed/vo/res/FeedResponseVo.java
@@ -0,0 +1,9 @@
+package way.presentation.feed.vo.res;
+
+public class FeedResponseVo {
+	public record SaveFeedResponse(
+		Long feedSeq
+	) {
+
+	}
+}
