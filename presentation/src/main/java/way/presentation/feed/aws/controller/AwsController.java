1b7ac58e18d00ead5b7436e4768f557fff7ab274
euics <pjh575157@gmail.com>
2024-07-21 23:52:07 +0900

    chore: Add CICD

--- presentation/src/main/java/way/presentation/feed/aws/controller/AwsController.java
+++ presentation/src/main/java/way/presentation/feed/aws/controller/AwsController.java
@@ -3,6 +3,7 @@ package way.presentation.feed.aws.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
@@ -11,6 +12,7 @@ import way.presentation.base.BaseResponse;
 
 @Tag(name = "Server Health Check", description = "서버 환경 확인을 위한 URL: https://wlrmadjel.com/actuator/health 반환값 200이면 Server OK")
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "https://wlrmadjel.com")})
+@RestController
 public class AwsController {
 	@GetMapping("/actuator/health")
 	public ResponseEntity<BaseResponse> healthCheck() {
