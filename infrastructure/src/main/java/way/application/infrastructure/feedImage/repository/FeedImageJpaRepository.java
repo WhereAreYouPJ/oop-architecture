6655500445ddbf1a8bd50f27bcc83ce5e11a8540
euics <pjh575157@gmail.com>
2024-07-21 23:12:49 +0900

    feat: FeedEntity 관련 삭제 메서드 추가 (#32)
    - deleteAllByFeedEntity 메서드 추가 및 구현
    - deleteAllByFeed 메서드 정의

--- infrastructure/src/main/java/way/application/infrastructure/feedImage/repository/FeedImageJpaRepository.java
+++ infrastructure/src/main/java/way/application/infrastructure/feedImage/repository/FeedImageJpaRepository.java
@@ -1,11 +1,13 @@
 package way.application.infrastructure.feedImage.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import way.application.infrastructure.feed.entity.FeedEntity;
 import way.application.infrastructure.feedImage.entity.FeedImageEntity;
 
 @Repository
 public interface FeedImageJpaRepository extends JpaRepository<FeedImageEntity, Long> {
-
+	void deleteAllByFeed(FeedEntity feedEntity);
 }
--- infrastructure/src/main/java/way/application/infrastructure/feedImage/repository/FeedImageRepository.java
+++ infrastructure/src/main/java/way/application/infrastructure/feedImage/repository/FeedImageRepository.java
@@ -1,7 +1,10 @@
 package way.application.infrastructure.feedImage.repository;
 
+import way.application.infrastructure.feed.entity.FeedEntity;
 import way.application.infrastructure.feedImage.entity.FeedImageEntity;
 
 public interface FeedImageRepository {
 	FeedImageEntity saveFeedImageEntity(FeedImageEntity feedImageEntity);
+
+	void deleteAllByFeedEntity(FeedEntity feedEntity);
 }
