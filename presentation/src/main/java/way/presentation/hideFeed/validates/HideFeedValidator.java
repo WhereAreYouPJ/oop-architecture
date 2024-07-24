bc9c1dd4595e2f9f87940a1d1ac9853f9009086a
euics <pjh575157@gmail.com>
2024-07-24 20:54:12 +0900

    feat: 피드 숨김 요청을 위한 HideFeedValidator 클래스 추가 (#48)
    - HideFeedRequest의 유효성 검사를 위한 HideFeedValidator 클래스 생성
    - validate 메소드에서 scheduleSeq와 memberSeq의 유효성을 각각 검증
    - scheduleSeq와 memberSeq가 null인 경우 BadRequestException 발생

---
+++ presentation/src/main/java/way/presentation/hideFeed/validates/HideFeedValidator.java
@@ -0,0 +1,29 @@
+package way.presentation.hideFeed.validates;
+
+import static way.presentation.hideFeed.vo.req.HideFeedRequestVo.*;
+
+import org.springframework.stereotype.Component;
+
+import way.application.utils.exception.BadRequestException;
+import way.application.utils.exception.ErrorResult;
+
+@Component
+public class HideFeedValidator {
+
+	public void validate(HideFeedRequest request) {
+		validateScheduleSeq(request.scheduleSeq());
+		validateMemberSeq(request.memberSeq());
+	}
+
+	private void validateScheduleSeq(Long scheduleSeq) {
+		if (scheduleSeq == null) {
+			throw new BadRequestException(ErrorResult.DTO_BAD_REQUEST_EXCEPTION);
+		}
+	}
+
+	private void validateMemberSeq(Long memberSeq) {
+		if (memberSeq == null) {
+			throw new BadRequestException(ErrorResult.DTO_BAD_REQUEST_EXCEPTION);
+		}
+	}
+}
