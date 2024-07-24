0150f1a631761d77fe16a6f847e381d90872d774
euics <pjh575157@gmail.com>
2024-07-24 21:07:37 +0900

    feat: 피드 삭제 기능 추가 (#48)
    - HideFeedService 클래스에 deleteHideFeed 메소드 추가
    - DeleteHideFeedRequestDto를 처리하여 피드를 삭제하는 로직 구현
    - 멤버와 일정을 조회하여 예외 처리
    - ScheduleEntity와 MemberEntity를 기준으로 숨김 피드를 전체 삭제

--- service/src/main/java/way/application/service/hideFeed/service/HideFeedService.java
+++ service/src/main/java/way/application/service/hideFeed/service/HideFeedService.java
@@ -10,6 +10,7 @@ import way.application.infrastructure.hideFeed.repository.HideFeedRepository;
 import way.application.infrastructure.member.entity.MemberEntity;
 import way.application.infrastructure.member.repository.MemberRepository;
 import way.application.infrastructure.schedule.entity.ScheduleEntity;
+import way.application.infrastructure.schedule.repository.ScheduleRepository;
 import way.application.infrastructure.scheduleMember.entity.ScheduleMemberEntity;
 import way.application.infrastructure.scheduleMember.repository.ScheduleMemberRepository;
 import way.application.service.hideFeed.mapper.HideFeedMapper;
@@ -20,25 +21,43 @@ public class HideFeedService {
 	private final HideFeedRepository hideFeedRepository;
 	private final MemberRepository memberRepository;
 	private final ScheduleMemberRepository scheduleMemberRepository;
+	private final ScheduleRepository scheduleRepository;
 
 	private final HideFeedMapper hideFeedMapper;
 
 	@Transactional
-	public void hideFeed(AddHideFeedRequestDto hideFeedRequestDto) {
+	public void addHideFeed(AddHideFeedRequestDto addHideFeedRequestDto) {
 		/*
 		 예외처리 (Repo 단)
 		 1. member 가 수락한 Schedule 인지 확인
 		 2. member 확인
 		*/
-		MemberEntity hideMemberEntity = memberRepository.findByMemberSeq(hideFeedRequestDto.memberSeq());
+		MemberEntity hideMemberEntity = memberRepository.findByMemberSeq(addHideFeedRequestDto.memberSeq());
+		scheduleRepository.findByScheduleSeq(addHideFeedRequestDto.scheduleSeq());
 		ScheduleMemberEntity scheduleMemberEntity
 			= scheduleMemberRepository.findAcceptedScheduleMemberByScheduleSeqAndMemberSeq(
-			hideFeedRequestDto.scheduleSeq(),
-			hideFeedRequestDto.memberSeq()
+			addHideFeedRequestDto.scheduleSeq(),
+			addHideFeedRequestDto.memberSeq()
 		);
 		ScheduleEntity hideScheduleEntity = scheduleMemberEntity.getSchedule();
 
 		// 저장
 		hideFeedRepository.save(hideFeedMapper.toHideFeedEntity(hideScheduleEntity, hideMemberEntity));
+	}
+
+	@Transactional
+	public void deleteHideFeed(DeleteHideFeedRequestDto deleteHideFeedRequestDto) {
+		// 예외처리 (Repo 단)
+		MemberEntity hideMemberEntity = memberRepository.findByMemberSeq(deleteHideFeedRequestDto.memberSeq());
+		scheduleRepository.findByScheduleSeq(deleteHideFeedRequestDto.hideScheduleSeq());
+		ScheduleMemberEntity scheduleMemberEntity
+			= scheduleMemberRepository.findAcceptedScheduleMemberByScheduleSeqAndMemberSeq(
+			deleteHideFeedRequestDto.hideScheduleSeq(),
+			deleteHideFeedRequestDto.memberSeq()
+		);
+		ScheduleEntity hideScheduleEntity = scheduleMemberEntity.getSchedule();
+
+		// 전체 삭제
+		hideFeedRepository.deleteAllByScheduleEntityAndMemberEntity(hideScheduleEntity, hideMemberEntity);
 	}
 }
