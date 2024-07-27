ec474306f0b0e1ed89acb753cfe5fb3d583463f3
euics <pjh575157@gmail.com>
2024-07-27 23:38:08 +0900

    remove: FeedMember 관련 클래스 삭제 (#60)

--- infrastructure/src/main/java/way/application/infrastructure/feedMember/repository/FeedMemberRepository.java
+++
@@ -1,9 +0,0 @@
-package way.application.infrastructure.feedMember.repository;
-
-import way.application.infrastructure.feed.entity.FeedEntity;
-import way.application.infrastructure.feedMember.entity.FeedMemberEntity;
-
-public interface FeedMemberRepository {
-	FeedMemberEntity saveFeedMemberEntity(FeedMemberEntity feedMemberEntity);
-	void deleteAllByFeedEntity(FeedEntity feedEntity);
-}
