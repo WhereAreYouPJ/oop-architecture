2555f2622ae202f1351bcdd3f4ea2124cef036f8
euics <pjh575157@gmail.com>
2024-07-28 16:37:27 +0900

    feat: BookMarkMapper 인터페이스 추가 (#63)
    - BookMarkMapper 인터페이스 정의: MapStruct를 사용하여 BookMarkEntity로 변환하는 매퍼
    - @Mapper 어노테이션 사용: componentModel을 "spring"으로 설정하여 스프링 컴포넌트로 등록
    - injectionStrategy를 CONSTRUCTOR로 설정하여 생성자 주입 사용
    - unmappedTargetPolicy를 IGNORE로 설정하여 매핑되지 않은 필드 무시
    - toBookMarkEntity 메서드 추가: FeedEntity와 MemberEntity를 BookMarkEntity로 매핑
      - bookMarkSeq 필드는 매핑에서 제외

---
+++ service/src/main/java/way/application/service/bookMark/mapper/BookMarkMapper.java
@@ -0,0 +1,18 @@
+package way.application.service.bookMark.mapper;
+
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+import way.application.infrastructure.bookMark.entity.BookMarkEntity;
+import way.application.infrastructure.feed.entity.FeedEntity;
+import way.application.infrastructure.member.entity.MemberEntity;
+
+@Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface BookMarkMapper {
+	@Mapping(target = "bookMarkSeq", ignore = true)
+	@Mapping(target = "feedEntity", source = "feedEntity")
+	@Mapping(target = "memberEntity", source = "memberEntity")
+	BookMarkEntity toBookMarkEntity(FeedEntity feedEntity, MemberEntity memberEntity);
+}
