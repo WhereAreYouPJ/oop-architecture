f292a40035ccb0ae5ec4cf1dbe9add4297fc1392
euics <pjh575157@gmail.com>
2024-07-28 15:53:31 +0900

    feat: GetHideFeedResponseDto 및 GetHideFeedResponse 추가 (#65)
    - GetHideFeedResponseDto 레코드 추가: 숨긴 피드 응답 DTO 정의
      - profileImage, startTime, location, title, feedImageUrl, content 필드 포함
      - TODO: bookMark 기능 추가 예정
    - GetHideFeedResponse 레코드 추가: 숨긴 피드 응답 VO 정의
      - profileImage, startTime, location, title, feedImageUrl, content 필드 포함
      - TODO: bookMark 기능 추가 예정

--- presentation/src/main/java/way/presentation/hideFeed/vo/res/HideFeedResponseVo.java
+++ presentation/src/main/java/way/presentation/hideFeed/vo/res/HideFeedResponseVo.java
@@ -1,8 +1,25 @@
 package way.presentation.hideFeed.vo.res;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class HideFeedResponseVo {
 	public record AddHideFeedResponse(
 		Long hideFeedSeq
+	) {
+
+	}
+
+	public record GetHideFeedResponse(
+		String profileImage,
+		LocalDateTime startTime,
+		String location,
+		String title,
+		List<String> feedImageUrl,
+		String content
+
+		// TODO: bookMark 기능 추가
+		// Boolean bookMark
 	) {
 
 	}
