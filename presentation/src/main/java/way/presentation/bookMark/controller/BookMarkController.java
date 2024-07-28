c77842b51c566f8fc2c16b3b53ebc46c5be4c7c7
euics <pjh575157@gmail.com>
2024-07-28 16:36:19 +0900

    feat: BookMarkController 클래스 추가 및 책갈피 추가 API 구현 (#63)
    - BookMarkController 클래스 정의: 책갈피 추가 기능을 위한 REST API 엔드포인트 제공
    - @RequestMapping("/book-mark"): 해당 경로에 대한 요청 처리
    - @PostMapping: 책갈피 추가 기능을 위한 POST 요청 처리 메서드 addHideFeed 추가
    - OpenAPI를 사용한 API 문서화 추가
      - 200: 요청 성공
      - 500: 서버 오류
      - 400: 요청 값 형식 오류, MEMBER_SEQ 오류, FEED_SEQ 오류, Member가 작성하지 않은 Feed 오류
      - 409: 이미 존재하는 Book Mark Feed 오류
    - 유효성 검사: AddBookMarkValidator를 사용하여 요청 값 검사
    - VO -> DTO 변환: AddBookMarRequest를 AddBookMarkRequestDto로 변환
    - 서비스 호출: BookMarkService를 사용하여 책갈피 추가 처리
    - DTO -> VO 변환: AddBookMarkResponseDto를 AddBookMarkResponse로 변환하여 응답
    - 응답 생성: BaseResponse를 사용하여 성공 응답 생성 및 반환

---
+++ presentation/src/main/java/way/presentation/bookMark/controller/BookMarkController.java
@@ -0,0 +1,96 @@
+package way.presentation.bookMark.controller;
+
+import static way.application.service.bookMark.dto.response.BookMarkResponseDto.*;
+import static way.presentation.bookMark.vo.req.BookMarkRequestVo.*;
+import static way.presentation.bookMark.vo.res.BookMarkResponseVo.*;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import way.application.service.bookMark.service.BookMarkService;
+import way.application.utils.exception.GlobalExceptionHandler;
+import way.presentation.base.BaseResponse;
+import way.presentation.bookMark.validates.AddBookMarkValidator;
+
+@RestController
+@RequestMapping("/book-mark")
+@RequiredArgsConstructor
+@Tag(name = "책갈피", description = "담당자 (박종훈)")
+public class BookMarkController {
+	private final AddBookMarkValidator addBookMarkValidator;
+
+	private final BookMarkService bookMarkService;
+
+	@PostMapping(name = "책갈피 추가")
+	@Operation(summary = "책갈피 추가 API", description = "책갈피 추가 API")
+	@ApiResponses(value = {
+		@ApiResponse(
+			responseCode = "200",
+			description = "요청에 성공하였습니다.",
+			useReturnTypeSchema = true),
+		@ApiResponse(
+			responseCode = "S500",
+			description = "500 SERVER_ERROR (나도 몰라 ..)",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(
+			responseCode = "B001",
+			description = "400 Invalid DTO Parameter errors / 요청 값 형식 요류",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(
+			responseCode = "MSB002",
+			description = "400 MEMBER_SEQ_BAD_REQUEST_EXCEPTION / MEMBER_SEQ 오류",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(
+			responseCode = "FSB019",
+			description = "400 FEED_SEQ_BAD_REQUEST_EXCEPTION / FEED_SEQ 오류",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(
+			responseCode = "BMFDC005",
+			description = "409 BOOK_MARK_FEED_DUPLICATION_CONFLICT_EXCEPTION / 이미 존재하는 Book Mark Feed 오류",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(
+			responseCode = "FDCBMB020",
+			description = "400 FEED_DIDNT_CREATED_BY_MEMBER_BAD_REQUEST_EXCEPTION / Member가 작성하지 않은 Feed 오류",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class)))
+	})
+	public ResponseEntity<BaseResponse<AddBookMarkResponse>> addHideFeed(
+		@Valid
+		@RequestBody AddBookMarRequest request
+	) {
+		// 유효성 검사
+		addBookMarkValidator.validate(request);
+
+		// VO -> DTO
+		AddBookMarkResponseDto addBookMarkResponseDto
+			= bookMarkService.addBookMarkFeed(request.toAddBookMarkRequestDto());
+
+		// DTO -> VO
+		AddBookMarkResponse response = new AddBookMarkResponse(addBookMarkResponseDto.bookMarkSeq());
+
+		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), response));
+	}
+}
