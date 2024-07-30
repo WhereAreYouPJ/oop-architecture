2680f7103a36c0612381c54aed0f4694ac65c8b4
euics <pjh575157@gmail.com>
2024-07-30 20:49:08 +0900

    feat: HideFeedEntity 조회 시 정렬 쿼리 생성 (#71)
    - ORDER BY startTime DESC

--- infrastructure/src/main/java/way/application/infrastructure/hideFeed/repository/HideFeedJpaRepository.java
+++ infrastructure/src/main/java/way/application/infrastructure/hideFeed/repository/HideFeedJpaRepository.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import way.application.infrastructure.feed.entity.FeedEntity;
@@ -18,5 +20,15 @@ public interface HideFeedJpaRepository extends JpaRepository<HideFeedEntity, Lon
 		MemberEntity memberEntity
 	);
 
-	Page<HideFeedEntity> findAllByMemberEntity(MemberEntity memberEntity, Pageable pageable);
+	@Query("""
+		SELECT h FROM HideFeedEntity h
+		JOIN h.feedEntity f
+		JOIN f.schedule s
+		WHERE h.memberEntity = :memberEntity
+		ORDER BY s.startTime DESC
+		""")
+	Page<HideFeedEntity> findAllByMemberEntityOrderByScheduleStartTimeDesc(
+		@Param("memberEntity") MemberEntity memberEntity,
+		Pageable pageable
+	);
 }
