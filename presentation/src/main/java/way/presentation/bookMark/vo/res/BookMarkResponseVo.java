e7b7e388b1b7f8de18699d1e16cd2a8236d0b28b
euics <pjh575157@gmail.com>
2024-07-31 19:15:30 +0900

    feat: 책갈피 조회 응답 DTO 및 VO 클래스 추가 (#73)
    - GetBookMarkResponse 클래스 추가
      - profileImage, startTime, location, title, feedImageUrl, content, bookMark 필드 포함
    - GetBookMarkResponseDto 클래스 추가
      - profileImage, startTime, location, title, feedImageUrl, content, bookMark 필드 포함

--- presentation/src/main/java/way/presentation/bookMark/vo/res/BookMarkResponseVo.java
+++ presentation/src/main/java/way/presentation/bookMark/vo/res/BookMarkResponseVo.java
@@ -1,8 +1,23 @@
 package way.presentation.bookMark.vo.res;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class BookMarkResponseVo {
 	public record AddBookMarkResponse(
 		Long bookMarkSeq
+	) {
+
+	}
+
+	public record GetBookMarkResponse(
+		String profileImage,
+		LocalDateTime startTime,
+		String location,
+		String title,
+		List<String> feedImageUrl,
+		String content,
+		Boolean bookMark
 	) {
 
 	}
