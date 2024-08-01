46c400ef7b6e2bf7584ed7aaafd140e061720bf9
euics <pjh575157@gmail.com>
2024-08-01 20:38:14 +0900

    feat: LocationEntity 조회를 위한 쿼리 메서드 추가 (#82)
    - findLocationSeqsByMemberEntity 메서드 추가
      - 특정 회원의 모든 위치 시퀀스를 조회하는 쿼리 정의
    - findLocationSeqsByLocationSeqs 메서드 추가
      - 특정 위치 시퀀스 목록에 해당하는 모든 위치 시퀀스를 조회하는 쿼리 정의
    - findAllByMemberEntityAndLocationSeqs 메서드 추가
      - 특정 회원과 위치 시퀀스 목록에 해당하는 모든 위치 엔티티를 조회하는 쿼리 정의

--- infrastructure/src/main/java/way/application/infrastructure/location/repository/LocationJpaRepository.java
+++ infrastructure/src/main/java/way/application/infrastructure/location/repository/LocationJpaRepository.java
@@ -1,10 +1,45 @@
 package way.application.infrastructure.location.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import way.application.infrastructure.location.entity.LocationEntity;
+import way.application.infrastructure.member.entity.MemberEntity;
 
 @Repository
 public interface LocationJpaRepository extends JpaRepository<LocationEntity, Long> {
+	@Query("""
+		SELECT 
+			l.locationSeq
+		FROM 
+			LocationEntity l
+		WHERE
+			l.memberEntity = :memberEntity
+		""")
+	List<Long> findLocationSeqsByMemberEntity(MemberEntity memberEntity);
+
+	@Query("""
+        SELECT 
+            l.locationSeq
+        FROM 
+            LocationEntity l
+        WHERE
+            l.locationSeq IN :locationSeqs
+        """)
+	List<Long> findLocationSeqsByLocationSeqs(List<Long> locationSeqs);
+
+	@Query("""
+		SELECT 
+			l
+		FROM 
+			LocationEntity l
+		WHERE 
+			l.memberEntity = :memberEntity 
+			AND 
+			l.locationSeq IN :locationSeqs
+		""")
+	List<LocationEntity> findAllByMemberEntityAndLocationSeqs(MemberEntity memberEntity, List<Long> locationSeqs);
 }
