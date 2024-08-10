f0c4b9906e1b148da95c91a259778ab31584a51a
euics <pjh575157@gmail.com>
2024-08-10 15:02:31 +0900

    feat: 친구 관계 확인 로직 추가 (#93)
    - `FriendDomain` 클래스에 친구 관계 확인 로직을 추가했습니다.
    - `checkFriends` 메서드를 구현하여 주어진 `MemberEntity` 리스트와 `FriendEntity` 리스트를 비교하여 모든 멤버가 친구 관계인지 확인합니다.
    - 모든 멤버가 친구가 아닌 경우, `NotFoundRequestException` 예외를 발생시키도록 처리했습니다.

---
+++ domain/src/main/java/way/application/domain/friend/FriendDomain.java
@@ -0,0 +1,26 @@
+package way.application.domain.friend;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import way.application.infrastructure.friend.entity.FriendEntity;
+import way.application.infrastructure.member.entity.MemberEntity;
+import way.application.utils.exception.ErrorResult;
+import way.application.utils.exception.NotFoundRequestException;
+
+@Component
+public class FriendDomain {
+	public void checkFriends(List<MemberEntity> memberEntities, List<FriendEntity> friendEntities) {
+		boolean friend = memberEntities.stream()
+			.allMatch(invitedMember ->
+				friendEntities.stream()
+					.anyMatch(friendEntity ->
+						friendEntity.getFriends().equals(invitedMember)
+					)
+			);
+
+		if (!friend)
+			throw new NotFoundRequestException(ErrorResult.FRIEND_NOT_FOUND_EXCEPTION);
+	}
+}
