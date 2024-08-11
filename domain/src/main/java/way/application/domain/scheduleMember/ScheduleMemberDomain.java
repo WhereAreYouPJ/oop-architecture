c90de59b6b705590277121d35a42ed2ad8c6ad98
euics <pjh575157@gmail.com>
2024-08-11 16:51:28 +0900

    feat: 특정 멤버에 대한 ScheduleMemberEntity 조회 기능 구현 (#92)
    - QueryDSL을 사용하여 특정 멤버와 관련된 ScheduleMemberEntity를 조회하는 findByMemberEntity 메서드 구현.
    - 초대된 멤버가 일정에 참여를 수락한 경우에만 해당 엔티티를 필터링.
    - 페이지네이션을 위해 조회된 결과를 PageImpl을 사용하여 반환.

--- domain/src/main/java/way/application/domain/scheduleMember/ScheduleMemberDomain.java
+++ domain/src/main/java/way/application/domain/scheduleMember/ScheduleMemberDomain.java
@@ -3,22 +3,50 @@ package way.application.domain.scheduleMember;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
+import com.querydsl.core.QueryResults;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
+import way.application.infrastructure.member.entity.MemberEntity;
+import way.application.infrastructure.schedule.entity.QScheduleEntity;
+import way.application.infrastructure.scheduleMember.entity.QScheduleMemberEntity;
 import way.application.infrastructure.scheduleMember.entity.ScheduleMemberEntity;
 
 @Component
+@RequiredArgsConstructor
 public class ScheduleMemberDomain {
+	private final JPAQueryFactory queryFactory;
 
-	/**
-	 * @param scheduleMemberEntities
-	 * @return
-	 *
-	 * ScheduleMemberEntity 로부터 UserName 값 추출 메서드
-	 */
 	public List<String> extractUserNameFromScheduleMemberEntities(List<ScheduleMemberEntity> scheduleMemberEntities) {
 		return scheduleMemberEntities.stream()
 			.map(sm -> sm.getInvitedMember().getUserName())
 			.collect(Collectors.toList());
+	}
+
+	public Page<ScheduleMemberEntity> findByMemberEntity(MemberEntity memberEntity, Pageable pageable) {
+		QScheduleMemberEntity scheduleMember = QScheduleMemberEntity.scheduleMemberEntity;
+		QScheduleEntity schedule = QScheduleEntity.scheduleEntity;
+
+		// QueryDSL을 사용하여 ScheduleMemberEntity를 조회
+		QueryResults<ScheduleMemberEntity> results = queryFactory
+			.selectFrom(scheduleMember)
+			.join(scheduleMember.schedule, schedule).fetchJoin()
+			.where(
+				scheduleMember.invitedMember.eq(memberEntity)
+					.and(scheduleMember.acceptSchedule.isTrue())
+			)
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize())
+			.fetchResults();
+
+		List<ScheduleMemberEntity> content = results.getResults();
+		long total = results.getTotal();
+
+		return new PageImpl<>(content, pageable, total);
 	}
 }
