dfd9a14ed5c513732cbd48e82c11ea902759ec09
euics <pjh575157@gmail.com>
2024-08-11 16:48:39 +0900

    feat: 피드 이미지 URL 조회 기능 구현 (#92)
    FeedImageDomain에 특정 피드에 속한 이미지 URL을 조회하는 findFeedImageURLsByFeedEntity 메서드 추가.
    JPAQueryFactory를 사용하여 QueryDSL 기반의 쿼리 작성.
    FeedEntity를 기준으로 관련된 이미지 URL들을 필터링하여 리스트로 반환.

--- domain/src/main/java/way/application/domain/feedImage/FeedImageDomain.java
+++ domain/src/main/java/way/application/domain/feedImage/FeedImageDomain.java
@@ -1,8 +1,27 @@
 package way.application.domain.feedImage;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
-@Component
-public class FeedImageDomain {
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
+import lombok.RequiredArgsConstructor;
+import way.application.infrastructure.feed.entity.FeedEntity;
+import way.application.infrastructure.feedImage.entity.QFeedImageEntity;
+
+@Component
+@RequiredArgsConstructor
+public class FeedImageDomain {
+	private final JPAQueryFactory queryFactory;
+
+	public List<String> findFeedImageURLsByFeedEntity(FeedEntity feedEntity) {
+		QFeedImageEntity feedImageEntity = QFeedImageEntity.feedImageEntity;
+
+		return queryFactory
+			.select(feedImageEntity.feedImageURL)
+			.from(feedImageEntity)
+			.where(feedImageEntity.feedEntity.eq(feedEntity))
+			.fetch();
+	}
 }
