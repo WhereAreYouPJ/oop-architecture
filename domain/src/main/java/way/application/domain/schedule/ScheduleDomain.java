726590019facbaf1f53923e53de95cd1120f8449
euics <pjh575157@gmail.com>
2024-08-13 18:28:07 +0900

    feat: 일정 시작 시간 유효성 검사 로직 추가 (#103)
    - `validateScheduleStartTime` 메서드를 통해 일정 시작 시간의 유효성을 검사하는 로직 추가.
    - 현재 시간으로부터 1시간 이전과 1시간 이후의 범위 내에 시작 시간이 포함될 경우 `BadRequestException`을 발생시키도록 구현.
    - 예외 발생 시 `ErrorResult.START_TIME_BAD_REQUEST_EXCEPTION`을 사용하여 적절한 오류 메시지와 상태 코드를 반환.

--- domain/src/main/java/way/application/domain/schedule/ScheduleDomain.java
+++ domain/src/main/java/way/application/domain/schedule/ScheduleDomain.java
@@ -2,15 +2,11 @@ package way.application.domain.schedule;
 
 import java.time.LocalDateTime;
 import java.time.YearMonth;
-import java.util.List;
-import java.util.stream.Collectors;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Component;
 
-import way.application.infrastructure.schedule.entity.ScheduleEntity;
-import way.application.infrastructure.scheduleMember.entity.ScheduleMemberEntity;
+import way.application.utils.exception.BadRequestException;
+import way.application.utils.exception.ErrorResult;
 
 @Component
 public class ScheduleDomain {
@@ -20,5 +16,16 @@ public class ScheduleDomain {
 
 	public LocalDateTime getEndOfMonth(YearMonth yearMonth) {
 		return yearMonth.atEndOfMonth().atTime(23, 59, 59);
+	}
+
+	public void validateScheduleStartTime(LocalDateTime startTime) {
+		LocalDateTime now = LocalDateTime.now();
+
+		LocalDateTime oneHourBefore = now.minusHours(1);
+		LocalDateTime oneHourAfter = now.plusHours(1);
+
+		if (startTime.isAfter(oneHourBefore) && startTime.isBefore(oneHourAfter)) {
+			throw new BadRequestException(ErrorResult.START_TIME_BAD_REQUEST_EXCEPTION);
+		}
 	}
 }
