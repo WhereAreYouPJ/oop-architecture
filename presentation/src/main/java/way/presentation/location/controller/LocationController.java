37482723dda8dd8037f7489b14ce8a7b673625d2
euics <pjh575157@gmail.com>
2024-08-13 23:39:47 +0900

    feat: 위치 즐겨찾기 조회 API 구현 (#105)
    - 위치 즐겨찾기 조회 기능을 제공하는 `getBookMark` 메서드 구현.
    - `@GetMapping`을 사용하여 위치 즐겨찾기 조회 요청을 처리하고, `memberSeq`의 유효성을 검사하는 로직 추가.
    - 다양한 예외 상황에 대해 `@ApiResponses`를 통해 응답 코드와 설명 정의.
    - 유효성 검사 후, 서비스 레이어에서 위치 정보를 조회하고, `GetLocationResponse`로 변환하여 응답으로 반환.
    - 성공 시 조회된 위치 즐겨찾기 정보와 함께 200 응답 반환.

--- presentation/src/main/java/way/presentation/location/controller/LocationController.java
+++ presentation/src/main/java/way/presentation/location/controller/LocationController.java
@@ -5,16 +5,21 @@ import static way.presentation.location.vo.req.LocationRequestVo.*;
 import static way.presentation.location.vo.res.LocationResponseVo.*;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -27,6 +32,7 @@ import way.application.utils.exception.GlobalExceptionHandler;
 import way.presentation.base.BaseResponse;
 import way.presentation.location.validates.AddLocationValidator;
 import way.presentation.location.validates.DeleteLocationValidator;
+import way.presentation.location.validates.GetLocationValidator;
 
 @RestController
 @RequestMapping("/location")
@@ -35,6 +41,7 @@ import way.presentation.location.validates.DeleteLocationValidator;
 public class LocationController {
 	private final AddLocationValidator addLocationValidator;
 	private final DeleteLocationValidator deleteLocationValidator;
+	private final GetLocationValidator getLocationValidator;
 
 	private final LocationService locationService;
 
@@ -122,5 +129,49 @@ public class LocationController {
 		locationService.deleteLocation(request.toDeleteLocationRequestDto());
 
 		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), "SUCCESS"));
+	}
+
+	@GetMapping(name = "위치 즐겨찾기 조회")
+	@Operation(summary = "위치 즐겨찾기 조회 API")
+	@ApiResponses(value = {
+		@ApiResponse(
+			responseCode = "200",
+			description = "요청에 성공하였습니다.",
+			useReturnTypeSchema = true),
+		@ApiResponse(
+			responseCode = "S500",
+			description = "500 SERVER_ERROR (나도 몰라 ..)",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(
+			responseCode = "B001",
+			description = "400 Invalid DTO Parameter errors / 요청 값 형식 요류",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(
+			responseCode = "MSB002",
+			description = "400 MEMBER_SEQ_BAD_REQUEST_EXCEPTION / MEMBER_SEQ 오류",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class)))
+	})
+	@Parameters({
+		@Parameter(name = "memberSeq", description = "회원 PK 값", example = "1"),
+	})
+	public ResponseEntity<BaseResponse<List<GetLocationResponse>>> getBookMark(
+		@Valid
+		@RequestParam(value = "memberSeq") Long memberSeq
+	) {
+		// Validate
+		getLocationValidator.validate(memberSeq);
+
+		List<GetLocationResponseDto> locationResponseDtos = locationService.getLocation(memberSeq);
+		List<GetLocationResponse> responses = locationResponseDtos.stream()
+			.map(dto -> new GetLocationResponse(dto.locationSeq(), dto.location(), dto.streetName()))
+			.toList();
+
+		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), responses));
 	}
 }
