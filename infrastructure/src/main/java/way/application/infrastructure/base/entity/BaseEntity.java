6d0ec08a497c3bddb9cd62aa39efaadee355ace8
euics <pjh575157@gmail.com>
2024-08-21 13:09:18 +0900

    feat: 공통 엔티티를 위한 BaseEntity 클래스 구현 (#112)
    - `BaseEntity` 클래스는 모든 엔티티의 기본 클래스 역할을 하도록 구현.
    - `@MappedSuperclass`를 사용하여 상속받는 엔티티들이 공통으로 사용할 수 있는 필드 제공.
    - `createdAt` 필드는 엔티티 생성 시 자동으로 타임스탬프가 기록되며, 수정 불가.
    - `updatedAt` 필드는 엔티티가 수정될 때마다 자동으로 타임스탬프가 업데이트됨.
    - `@CreationTimestamp`와 `@UpdateTimestamp`를 통해 Hibernate에서 자동으로 타임스탬프 처리.
    - `@EntityListeners(AuditingEntityListener.class)`를 통해 JPA 감사 기능 활성화.

---
+++ infrastructure/src/main/java/way/application/infrastructure/base/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package way.application.infrastructure.base.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+	@CreationTimestamp
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
+	@UpdateTimestamp
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+
+}
