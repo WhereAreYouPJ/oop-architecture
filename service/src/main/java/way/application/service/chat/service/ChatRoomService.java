9f6ae7f18e7ae60e74fe768ab5954b0221a42242
euics <pjh575157@gmail.com>
2024-08-21 13:14:47 +0900

    feat: 채팅방 서비스 로직 구현 (#112)
    - `ChatRoomService` 클래스에 채팅방 생성, 입장, 메시지 전송 기능을 처리하는 서비스 로직 구현.
    - `createChatRoom` 메서드:
      - UUID를 사용해 고유한 채팅방 ID를 생성하고, `ChatRoomEntity`를 저장.
      - 저장된 채팅방의 ID를 `CreateChatRoomResponseDto`로 반환.
    
    - `enterChatRoom` 메서드:
      - 멤버, 일정, 채팅방의 유효성을 검사하고, 이미 채팅방에 멤버가 존재하는지 확인.
      - `ChatRoomMemberEntity`를 생성하고 저장하여 멤버가 채팅방에 입장하도록 처리.
    
    - `createChat` 메서드:
      - 채팅방과 멤버의 유효성을 검사하고, 멤버가 채팅방에 존재하는지 확인.
      - `ChatEntity`를 생성하고 저장하여 채팅 메시지를 데이터베이스에 기록.
    
    - 각 메서드는 필요한 경우 예외를 발생시켜 유효성 검사를 처리하고, 성공 시 데이터를 저장하고 응답을 반환.

---
+++ service/src/main/java/way/application/service/chat/service/ChatRoomService.java
@@ -0,0 +1,98 @@
+package way.application.service.chat.service;
+
+import static way.application.service.chat.dto.request.ChatRoomRequestDto.*;
+import static way.application.service.chat.dto.response.ChatRoomResponseDto.*;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import way.application.infrastructure.jpa.chatRoom.entity.ChatRoomEntity;
+import way.application.infrastructure.jpa.chatRoom.entity.ChatRoomMemberEntity;
+import way.application.infrastructure.jpa.chatRoom.repository.ChatRoomMemberRepository;
+import way.application.infrastructure.jpa.chatRoom.repository.ChatRoomRepository;
+import way.application.infrastructure.jpa.member.entity.MemberEntity;
+import way.application.infrastructure.jpa.member.repository.MemberRepository;
+import way.application.infrastructure.jpa.schedule.entity.ScheduleEntity;
+import way.application.infrastructure.jpa.schedule.repository.ScheduleRepository;
+import way.application.infrastructure.jpa.scheduleMember.repository.ScheduleMemberRepository;
+import way.application.infrastructure.mongo.chat.documents.ChatEntity;
+import way.application.infrastructure.mongo.chat.repository.ChatRepository;
+import way.application.service.chat.mapper.ChatMapper;
+import way.application.service.chat.mapper.ChatRoomMapper;
+import way.application.service.chat.mapper.ChatRoomMemberMapper;
+
+@Service
+@RequiredArgsConstructor
+public class ChatRoomService {
+	private final ChatRoomRepository chatRoomRepository;
+	private final MemberRepository memberRepository;
+	private final ScheduleRepository scheduleRepository;
+	private final ScheduleMemberRepository scheduleMemberRepository;
+	private final ChatRoomMemberRepository chatRoomMemberRepository;
+	private final ChatRepository chatRepository;
+
+	private final ChatRoomMapper chatRoomMapper;
+	private final ChatRoomMemberMapper chatRoomMemberMapper;
+	private final ChatMapper chatMapper;
+
+	@Transactional
+	public CreateChatRoomResponseDto createChatRoom(CreateChatRoomRequestDto createChatRoomRequestDto) {
+		// Chat Room Entity 생성 및 저장
+		ChatRoomEntity chatRoomEntity = chatRoomMapper.toChatRoomEntity(
+			UUID.randomUUID().toString(),
+			createChatRoomRequestDto.roomName()
+		);
+
+		ChatRoomEntity savedChatRoomEntity = chatRoomRepository.saveChatRoomEntity(chatRoomEntity);
+
+		return new CreateChatRoomResponseDto(savedChatRoomEntity.getChatRoomSeq());
+	}
+
+	@Transactional
+	public void enterChatRoom(EnterChatRoomRequestDto enterChatRoomRequestDto) {
+		/*
+		 1. Member 유효성 검사
+		 2. Schedule 유효성 검사
+		 3. Scheulde Member 유효성 검사
+		 4. Chat Room 유효성 검사
+		 5. 이미 Chat Room Member 에 존재
+		*/
+		MemberEntity memberEntity = memberRepository.findByMemberSeq(enterChatRoomRequestDto.memberSeq());
+		scheduleRepository.findByScheduleSeq(enterChatRoomRequestDto.scheduleSeq());
+		ScheduleEntity scheduleEntity = scheduleMemberRepository.findAcceptedScheduleMemberInSchedule(
+			enterChatRoomRequestDto.scheduleSeq(),
+			enterChatRoomRequestDto.memberSeq()
+		).getSchedule();
+		ChatRoomEntity chatRoomEntity = chatRoomRepository.findByChatRoomSeq(enterChatRoomRequestDto.chatRoomSeq());
+		chatRoomMemberRepository.existsChatRoomMemberEntity(memberEntity, chatRoomEntity);
+
+		// Chat Room Member Entity 생성 및 저장
+		ChatRoomMemberEntity chatRoomMemberEntity
+			= chatRoomMemberMapper.toChatRoomMemberEntity(memberEntity, chatRoomEntity, scheduleEntity);
+		chatRoomMemberRepository.saveChatRoomMemberEntity(chatRoomMemberEntity);
+	}
+
+	@Transactional
+	public void createChat(SendChatRequestDto sendChatRequestDto) {
+		/*
+		 1. Chat Room 유효성 검사
+		 2. Member 유효성 검사
+		 3. Chat Room 안에 Member 존재 유효성 검사
+		*/
+		ChatRoomEntity chatRoomEntity = chatRoomRepository.findByChatRoomSeq(sendChatRequestDto.chatRoomSeq());
+		MemberEntity memberEntity = memberRepository.findByMemberSeq(sendChatRequestDto.senderMemberSeq());
+		chatRoomMemberRepository.existsChatRoomMemberEntity(memberEntity, chatRoomEntity);
+
+		// Chat Entity 생성 및 저장
+		ChatEntity chatEntity = chatMapper.toChatEntity(
+			chatRoomEntity,
+			memberEntity,
+			sendChatRequestDto.message()
+		);
+
+		chatRepository.saveChatEntity(chatEntity);
+	}
+}
