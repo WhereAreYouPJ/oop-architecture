d4c109e83c43bd5b85aa62ccc7682b01f9abc548
euics <pjh575157@gmail.com>
2024-08-21 13:11:17 +0900

    feat: 채팅방 생성 및 입장 API 구현 (#112)
    - `ChatRoomController` 클래스에 채팅방 생성 및 입장 API 구현.
    - `@PostMapping`을 사용하여 채팅방 생성과 입장 요청을 처리.
    - `createChatRoom` 메서드는 채팅방 생성 요청을 받아 유효성 검사를 수행한 후, `ChatRoomService`를 호출하여 채팅방을 생성하고, 결과를 반환.
    - `enterChatRoom` 메서드는 채팅방 입장 요청을 받아 유효성 검사를 수행한 후, `ChatRoomService`를 호출하여 사용자가 채팅방에 입장하도록 처리.
    - 다양한 예외 상황에 대한 `@ApiResponses`를 통해 응답 코드와 설명을 정의하여, 클라이언트에게 명확한 피드백을 제공.
    - 성공 시 `BaseResponse`를 사용해 응답을 표준화하고 200 상태 코드와 함께 결과를 반환.

---
+++ presentation/src/main/java/way/presentation/chat/controller/ChatRoomController.java
@@ -0,0 +1,118 @@
+package way.presentation.chat.controller;
+
+import static way.application.service.chat.dto.response.ChatRoomResponseDto.*;
+import static way.presentation.chat.vo.request.ChatRoomRequestVo.*;
+import static way.presentation.chat.vo.response.ChatRoomResponseVo.*;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import way.application.service.chat.service.ChatRoomService;
+import way.application.utils.exception.GlobalExceptionHandler;
+import way.presentation.base.BaseResponse;
+import way.presentation.chat.validates.CreateChatRoomValidator;
+import way.presentation.chat.validates.EnterChatRoomValidator;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/chatroom")
+@Tag(name = "채팅", description = "담당자 (박종훈)")
+public class ChatRoomController {
+	private final CreateChatRoomValidator createChatRoomValidator;
+	private final EnterChatRoomValidator enterChatRoomValidator;
+
+	private final ChatRoomService chatRoomService;
+
+	// 채팅방 생성
+	@PostMapping()
+	@Operation(summary = "채팅방 생성 API", description = "일정 생성될 시 채팅방 생성 API 입니다. 순수 채팅방 생성 API입니다.")
+	public ResponseEntity<BaseResponse<CreateChatRoomResponse>> createChatRoom(
+		@Valid
+		@RequestBody CreateChatRoomRequest request
+	) {
+		// 유효성 검사
+		createChatRoomValidator.validate(request);
+
+		CreateChatRoomResponseDto createChatRoomResponseDto
+			= chatRoomService.createChatRoom(request.toCreateChatRoomRequestDto());
+
+		CreateChatRoomResponse response = new CreateChatRoomResponse(createChatRoomResponseDto.chatRoomSeq());
+
+		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), response));
+	}
+
+	// 채팅방 입장
+	@PostMapping("/enter")
+	@Operation(summary = "채팅방 입장 API", description = "채팅방 생성 이후, Schedule Accept true API 이후 채팅방 입장")
+	@ApiResponses(value = {
+		@ApiResponse(
+			responseCode = "200",
+			description = "요청에 성공하였습니다.",
+			useReturnTypeSchema = true),
+		@ApiResponse(
+			responseCode = "S500",
+			description = "500 SERVER_ERROR",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(
+			responseCode = "B001",
+			description = "400 Invalid DTO Parameter errors",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(
+			responseCode = "MSB002",
+			description = "400 MEMBER_SEQ_BAD_REQUEST_EXCEPTION",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(
+			responseCode = "SSB003",
+			description = "400 SCHEDULE_SEQ_BAD_REQUEST_EXCEPTION",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(
+			responseCode = "MSNISB004",
+			description = "400 MEMBER_SEQ_NOT_IN_SCHEDULE_BAD_REQUEST_EXCEPTION",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(
+			responseCode = "CRIB031",
+			description = "400 CHAT_ROOM_ID_BAD_REQUEST_EXCEPTION",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+		@ApiResponse(
+			responseCode = "CRMDC006",
+			description = "409 CHAT_ROOM_MEMBER_DUPLICATION_CONFLICT_EXCEPTION",
+			content = @Content(
+				schema = @Schema(
+					implementation = GlobalExceptionHandler.ErrorResponse.class))),
+	})
+	public ResponseEntity<BaseResponse> enterChatRoom(
+		@Valid
+		@RequestBody EnterChatRoomRequest request
+	) {
+		// 유효성 검사
+		enterChatRoomValidator.validate(request);
+
+		chatRoomService.enterChatRoom(request.toEnterChatRoomRequestDto());
+
+		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), "SUCCESS"));
+	}
+}
