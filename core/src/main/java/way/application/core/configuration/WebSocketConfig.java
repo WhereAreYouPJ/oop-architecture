00cfcd69f07f7d1525332c7f0be8279bdb7a6ddc
euics <pjh575157@gmail.com>
2024-08-23 15:16:31 +0900

    hotfix: WebSocket Error

--- core/src/main/java/way/application/core/configuration/WebSocketConfig.java
+++ core/src/main/java/way/application/core/configuration/WebSocketConfig.java
@@ -52,7 +52,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/ws")
-			.setAllowedOriginPatterns("*")
-			.withSockJS();
+			.setAllowedOriginPatterns("*");
 	}
 }
