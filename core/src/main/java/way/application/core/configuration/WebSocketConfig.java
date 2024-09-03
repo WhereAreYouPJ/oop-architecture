a2f6f27af7844b769fa606697605f59a86d5982f
euics <pjh575157@gmail.com>
2024-09-03 23:59:42 +0900

    hotfix: WebSocket 오류 해결

--- core/src/main/java/way/application/core/configuration/WebSocketConfig.java
+++ core/src/main/java/way/application/core/configuration/WebSocketConfig.java
@@ -29,6 +29,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/ws")
+			.setAllowedOriginPatterns("https://wlrmadjel.com")
 			.setAllowedOriginPatterns("*");
 		//.withSockJS();
 	}
