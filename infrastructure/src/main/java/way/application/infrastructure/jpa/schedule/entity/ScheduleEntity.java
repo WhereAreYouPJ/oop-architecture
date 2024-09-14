a697a9655b8fc86a18ff400ecad823e81b25d3d5
euics <pjh575157@gmail.com>
2024-09-14 14:04:03 +0900

    feat: 스케줄 엔티티 생성 및 속성 정의 (#122)
    - `ScheduleEntity` 클래스 추가
      - 스케줄 정보를 저장하는 엔티티
      - `scheduleSeq`: 스케줄의 고유 식별자 (자동 증가 값)
      - `title`: 스케줄 제목 (필수)
      - `startTime`, `endTime`: 스케줄 시작 및 종료 시간 (필수)
      - `location`: 스케줄 장소 (선택)
      - `streetName`: 장소의 도로명 주소 (선택)
      - `x`, `y`: 장소의 좌표 (선택)
      - `color`: 스케줄 색상, 기본값은 `'Color_7B50FF'`
      - `memo`: 스케줄 메모 (선택, LOB 타입)
      - `allDay`: 하루 종일 여부, 기본값은 `false`
    
    - `@DynamicInsert`, `@DynamicUpdate` 사용하여 필요한 필드만 insert, update 하도록 설정

--- infrastructure/src/main/java/way/application/infrastructure/jpa/schedule/entity/ScheduleEntity.java
+++ infrastructure/src/main/java/way/application/infrastructure/jpa/schedule/entity/ScheduleEntity.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -30,19 +31,34 @@ public class ScheduleEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long scheduleSeq;
 
+	@Column(name = "title", nullable = false)
 	private String title;
 
+	@Column(name = "start_time", nullable = false)
 	private LocalDateTime startTime;
+
+	@Column(name = "end_time", nullable = false)
 	private LocalDateTime endTime;
 
+	@Column(name = "location", nullable = true)
 	private String location;
+
+	@Column(name = "street_name", nullable = true)
 	private String streetName;
 
+	@Column(name = "x", nullable = true)
 	private Double x;
+
+	@Column(name = "y", nullable = true)
 	private Double y;
 
+	@Column(name = "color", nullable = false, columnDefinition = "varchar(255) default 'Color_7B50FF'")
 	private String color;
 
-	@Column(length = 5000)
+	@Lob
+	@Column(name = "memo", nullable = true)
 	private String memo;
+
+	@Column(name = "all_day", columnDefinition = "boolean default false")
+	private Boolean allDay;
 }
