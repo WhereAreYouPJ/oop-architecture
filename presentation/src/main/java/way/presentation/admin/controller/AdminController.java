8beda836134b60b9fd1f9010b52c4a2d09e163e5
euics <pjh575157@gmail.com>
2024-09-19 17:33:19 +0900

    feat: 관리자 홈 이미지 등록, 조회, 삭제 기능 구현
    - 홈 이미지 등록 기능 추가: 이미지 업로드 요청을 처리하고, 성공 시 이미지 고유 번호를 응답으로 반환
    - 홈 이미지 조회 기능 추가: 특정 이미지 번호로 이미지 URL을 조회하여 응답으로 반환
    - 홈 이미지 삭제 기능 추가: 요청된 이미지 번호로 이미지를 삭제하고 성공 메시지 반환
    - 각각의 요청 처리 시 BaseResponse로 성공 상태와 함께 응답 반환
    - AdminService를 통해 비즈니스 로직 처리
    - Swagger를 통해 API 문서화 (담당자: 박종훈)
    
    관련 이슈: #144

---
+++ presentation/src/main/java/way/presentation/admin/controller/AdminController.java
@@ -0,0 +1,64 @@
+package way.presentation.admin.controller;
+
+import static way.application.service.admin.dto.request.AdminRequestDto.*;
+import static way.application.service.admin.dto.response.AdminResponseDto.*;
+import static way.presentation.admin.vo.request.AdminRequestVo.*;
+import static way.presentation.admin.vo.response.AdminResponseVo.*;
+
+import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import way.application.service.admin.service.AdminService;
+import way.presentation.base.BaseResponse;
+
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+@Tag(name = "관리자", description = "담당자 (박종훈)")
+public class AdminController {
+	private final AdminService adminService;
+
+	@PostMapping("/image")
+	public ResponseEntity<BaseResponse<AddHomeImageResponse>> addHomeImage(
+		@ModelAttribute AddHomeImageRequest request
+	) throws IOException {
+		AddHomeImageResponseDto addHomeImageResponseDto = adminService.addHomeImage(request.toAddHomeImageRequestDto());
+
+		AddHomeImageResponse response = new AddHomeImageResponse(addHomeImageResponseDto.adminImageSeq());
+
+		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), response));
+	}
+
+	@GetMapping("/image")
+	public ResponseEntity<BaseResponse<GetHomeImageResponse>> getHomeImage(
+		@RequestParam(value = "adminImageSeq") Long adminImageSeq
+	) {
+		GetHomeImageResponseDto getHomeImageResponseDto
+			= adminService.getHomeImage(new GetHomeImageRequestDto(adminImageSeq));
+
+		GetHomeImageResponse response = new GetHomeImageResponse(getHomeImageResponseDto.imageURL());
+
+		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), response));
+	}
+
+	@DeleteMapping("/image")
+	public ResponseEntity<BaseResponse> deleteHomeImage(
+		@RequestBody DeleteHomeImageRequest request
+	) {
+		adminService.deleteHomeImage(request.toDeleteHomeImageRequestDto());
+
+		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), "SUCCESS"));
+	}
+}
