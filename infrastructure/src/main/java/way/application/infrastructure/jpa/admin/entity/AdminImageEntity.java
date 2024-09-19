3f34459dd3abd143607508f80c29a6ead22b2e49
euics <pjh575157@gmail.com>
2024-09-19 17:33:54 +0900

    feat: 관리자 이미지 엔티티 구현
    - AdminImageEntity 클래스 생성
    - admin_image_seq를 기본 키로 사용하여 자동 생성되도록 설정
    - imageURL 필드 추가, 이미지 URL을 저장하며 null을 허용하지 않음
    - @DynamicInsert와 @DynamicUpdate 어노테이션 사용으로 변경된 필드만 insert/update
    - @Builder, @Getter, @AllArgsConstructor, @NoArgsConstructor 적용으로 엔티티 생성과 데이터 접근 용이
    
    관련 이슈: #144

---
+++ infrastructure/src/main/java/way/application/infrastructure/jpa/admin/entity/AdminImageEntity.java
@@ -0,0 +1,33 @@
+package way.application.infrastructure.jpa.admin.entity;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ADMIN")
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamicInsert
+@DynamicUpdate
+@Getter
+@Builder(toBuilder = true)
+public class AdminImageEntity {
+	@Id
+	@Column(name = "admin_image_seq")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long adminImageSeq;
+
+	@Column(name = "image_url", nullable = false)
+	public String imageURL;
+}
