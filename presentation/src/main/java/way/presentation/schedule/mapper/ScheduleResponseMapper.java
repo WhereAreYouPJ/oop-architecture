c2092fcc5ec596136003950dbe72299d060e7895
euics <pjh575157@gmail.com>
2024-09-27 03:28:02 +0900

    feat: Add ScheduleResponseMapper (#153)
    - DTO -> VO Mapper 추가
       - Service에서 Presentation를 의존할 수 없기 때문에 Response Mapper 추가

---
+++ presentation/src/main/java/way/presentation/schedule/mapper/ScheduleResponseMapper.java
@@ -0,0 +1,14 @@
+package way.presentation.schedule.mapper;
+
+import static way.application.service.schedule.dto.response.ScheduleResponseDto.*;
+import static way.presentation.schedule.vo.response.ScheduleResponseVo.*;
+
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface ScheduleResponseMapper {
+	SaveScheduleResponse toSaveScheduleResponse(SaveScheduleResponseDto responseDto);
+}
