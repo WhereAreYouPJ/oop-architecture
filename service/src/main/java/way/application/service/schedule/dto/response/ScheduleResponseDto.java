00432c3b3dd1ebd451bc0ac1a6f2967a631405d4
euics <pjh575157@gmail.com>
2024-09-27 23:47:23 +0900

    feat: 일정 상세 조회 API 수정 (#151)
    - Response 시 allDay 변수 추가

--- service/src/main/java/way/application/service/schedule/dto/response/ScheduleResponseDto.java
+++ service/src/main/java/way/application/service/schedule/dto/response/ScheduleResponseDto.java
@@ -29,6 +29,7 @@ public class ScheduleResponseDto {
 		Double y,
 		String color,
 		String memo,
+		Boolean allDay,
 		List<GetScheduleMemberInfoDto> memberInfos
 	) implements Serializable {
 		private static final long serialVersionUID = 1L;
