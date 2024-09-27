355568aaac40c6a73d06439170a3c66721c316d6
euics <pjh575157@gmail.com>
2024-09-28 00:58:53 +0900

    feat: FeedEntityMapper에 SaveFeedResponseDto 매핑 로직 추가 (#157)
    - FeedEntityMapper에 FeedEntity를 SaveFeedResponseDto로 변환하는 메서드 추가
    - 기존 FeedEntity 매핑 로직을 유지하며 DTO 변환 기능 확장

--- service/src/main/java/way/application/service/feed/mapper/FeedEntityMapper.java
+++ service/src/main/java/way/application/service/feed/mapper/FeedEntityMapper.java
@@ -1,5 +1,7 @@
 package way.application.service.feed.mapper;
 
+import static way.application.service.feed.dto.response.FeedResponseDto.*;
+
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -10,11 +12,13 @@ import way.application.infrastructure.jpa.member.entity.MemberEntity;
 import way.application.infrastructure.jpa.schedule.entity.ScheduleEntity;
 
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR, unmappedTargetPolicy = ReportingPolicy.IGNORE)
-public interface FeedMapper {
+public interface FeedEntityMapper {
 	@Mapping(target = "feedSeq", ignore = true)
 	@Mapping(target = "schedule", source = "schedule")
 	@Mapping(target = "creatorMember", source = "creatorMember")
 	@Mapping(target = "title", source = "title")
 	@Mapping(target = "content", source = "content")
 	FeedEntity toFeedEntity(ScheduleEntity schedule, MemberEntity creatorMember, String title, String content);
+
+	SaveFeedResponseDto toSaveFeedResponseDto(FeedEntity feedEntity);
 }
