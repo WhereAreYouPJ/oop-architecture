6617a0cf6b2782dc3edd18694a9ba4fcb4795f2f
euics <pjh575157@gmail.com>
2024-10-05 16:58:05 +0900

    hotfix: FeedController Swagger MultipartFile 오류 수정 (#161)

--- presentation/src/main/java/way/presentation/feed/controller/FeedController.java
+++ presentation/src/main/java/way/presentation/feed/controller/FeedController.java
@@ -5,19 +5,22 @@ import static way.presentation.feed.vo.request.FeedRequestVo.*;
 import static way.presentation.feed.vo.response.FeedResponseVo.*;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -42,7 +45,7 @@ public class FeedController {
 	private final FeedResponseMapper feedResponseMapper;
 	private final FeedService feedService;
 
-	@PostMapping(name = "피드 생성")
+	@PostMapping(name = "피드 생성", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	@Operation(summary = "피드 생성 API")
 	@ApiResponses(value = {
 		@ApiResponse(responseCode = "200", description = "200 요청에 성공하였습니다.", useReturnTypeSchema = true),
@@ -55,17 +58,18 @@ public class FeedController {
 	})
 	public ResponseEntity<BaseResponse<SaveFeedResponse>> createFeed(
 		@Valid
-		@ModelAttribute SaveFeedRequest request
+		@RequestPart(value = "feedImageList", required = false) List<MultipartFile> feedImageList,
+		@RequestPart SaveFeedRequest request
 	) throws IOException {
 		request.saveFeedRequestValidate();
 
-		SaveFeedResponseDto responseDto = feedService.saveFeed(request.toSaveFeedRequestDto());
+		SaveFeedResponseDto responseDto = feedService.saveFeed(request.toSaveFeedRequestDto(), feedImageList);
 		SaveFeedResponse response = feedResponseMapper.toSaveFeedResponse(responseDto);
 
 		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), response));
 	}
 
-	@PutMapping(name = "피드 수정")
+	@PutMapping(name = "피드 수정", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	@Operation(summary = "피드 수정 API")
 	@ApiResponses(value = {
 		@ApiResponse(responseCode = "200", description = "200 요청에 성공하였습니다.", useReturnTypeSchema = true),
@@ -77,11 +81,12 @@ public class FeedController {
 	})
 	public ResponseEntity<BaseResponse<ModifyFeedResponse>> modifyFeed(
 		@Valid
-		@ModelAttribute ModifyFeedRequest request
+		@RequestPart(value = "feedImageList", required = false) List<MultipartFile> feedImageList,
+		@RequestPart ModifyFeedRequest request
 	) throws IOException {
 		request.modifyFeedRequestValidator();
 
-		ModifyFeedResponseDto responseDto = feedService.modifyFeed(request.toModifyFeedRequestDto());
+		ModifyFeedResponseDto responseDto = feedService.modifyFeed(request.toModifyFeedRequestDto(), feedImageList);
 		ModifyFeedResponse response = feedResponseMapper.toModifyFeedResponse(responseDto);
 
 		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), response));
