09a3333990370ae734cd791e92fa9bdc0b29b63a
euics <pjh575157@gmail.com>
2024-10-07 01:06:35 +0900

    feat: LocationMapper와 LocationEntityMapper 통합 및 매핑 메서드 추가 (#173)
    - LocationMapper와 LocationEntityMapper를 통합하여 매퍼 중복을 제거하고 관리 효율성 향상
    - `toAddLocationResponseDto`, `toGetLocationResponseDto` 메서드를 추가하여 LocationEntity의 응답 변환 로직을 매퍼로 이동
    - 기존의 `LocationMapper` 인터페이스를 `LocationEntityMapper`로 대체하여 매핑의 일관성 유지

--- service/src/main/java/way/application/service/location/mapper/LocationEntityMapper.java
+++ service/src/main/java/way/application/service/location/mapper/LocationEntityMapper.java
@@ -1,5 +1,7 @@
 package way.application.service.location.mapper;
 
+import static way.application.service.location.dto.response.LocationResponseDto.*;
+
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -9,10 +11,14 @@ import way.application.infrastructure.jpa.location.entity.LocationEntity;
 import way.application.infrastructure.jpa.member.entity.MemberEntity;
 
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR, unmappedTargetPolicy = ReportingPolicy.IGNORE)
-public interface LocationMapper {
+public interface LocationEntityMapper {
 	@Mapping(target = "locationSeq", ignore = true)
 	@Mapping(target = "memberEntity", source = "memberEntity")
 	@Mapping(target = "location", source = "location")
 	@Mapping(target = "streetName", source = "streetName")
 	LocationEntity toLocationMapper(MemberEntity memberEntity, String location, String streetName);
+
+	AddLocationResponseDto toAddLocationResponseDto(LocationEntity locationEntity);
+
+	GetLocationResponseDto toGetLocationResponseDto(LocationEntity locationEntity);
 }
