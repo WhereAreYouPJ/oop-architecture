183ca372a354d2ea56624b57714bde2523812166
euics <pjh575157@gmail.com>
2024-10-06 21:49:32 +0900

    refactor: ChatRoomService 내 생성 메서드 제거 및 코드 간소화 (#167)
    - `createChatRoom` 메서드 제거 및 관련 코드를 축소하여 코드 유지보수를 용이하게 함.
    - `EnterChatRoomRequestDto` 및 `SendChatRequestDto`에 대해 메서드 파라미터 이름을 더 간단하게 변경.

--- service/src/main/java/way/application/service/chat/service/ChatRoomService.java
+++ service/src/main/java/way/application/service/chat/service/ChatRoomService.java
@@ -15,13 +15,11 @@ import way.application.infrastructure.jpa.chatRoom.repository.ChatRoomMemberRepo
 import way.application.infrastructure.jpa.chatRoom.repository.ChatRoomRepository;
 import way.application.infrastructure.jpa.member.entity.MemberEntity;
 import way.application.infrastructure.jpa.member.repository.MemberRepository;
-import way.application.infrastructure.jpa.schedule.entity.ScheduleEntity;
 import way.application.infrastructure.jpa.schedule.repository.ScheduleRepository;
 import way.application.infrastructure.jpa.scheduleMember.repository.ScheduleMemberRepository;
 import way.application.infrastructure.mongo.chat.documents.ChatEntity;
 import way.application.infrastructure.mongo.chat.repository.ChatRepository;
 import way.application.service.chat.mapper.ChatMapper;
-import way.application.service.chat.mapper.ChatRoomMapper;
 import way.application.service.chat.mapper.ChatRoomMemberMapper;
 
 @Service
@@ -34,32 +32,11 @@ public class ChatRoomService {
 	private final ChatRoomMemberRepository chatRoomMemberRepository;
 	private final ChatRepository chatRepository;
 
-	private final ChatRoomMapper chatRoomMapper;
 	private final ChatRoomMemberMapper chatRoomMemberMapper;
 	private final ChatMapper chatMapper;
 
 	@Transactional
-	public CreateChatRoomResponseDto createChatRoom(CreateChatRoomRequestDto createChatRoomRequestDto) {
-		/*
-		 1. Schedule 유효성 검사
-		 2. Chat Room 이미 존재
-		*/
-		ScheduleEntity scheduleEntity = scheduleRepository.findByScheduleSeq(createChatRoomRequestDto.scheduleSeq());
-		chatRoomRepository.existChatRoomEntityByScheduleEntity(scheduleEntity);
-
-		// Chat Room Entity 생성 및 저장
-		ChatRoomEntity chatRoomEntity = chatRoomMapper.toChatRoomEntity(
-			UUID.randomUUID().toString(),
-			scheduleEntity
-		);
-
-		ChatRoomEntity savedChatRoomEntity = chatRoomRepository.saveChatRoomEntity(chatRoomEntity);
-
-		return new CreateChatRoomResponseDto(savedChatRoomEntity.getChatRoomSeq());
-	}
-
-	@Transactional
-	public void enterChatRoom(EnterChatRoomRequestDto enterChatRoomRequestDto) {
+	public void enterChatRoom(EnterChatRoomRequestDto requestDto) {
 		/*
 		 1. Member 유효성 검사
 		 2. Schedule 유효성 검사
@@ -67,13 +44,10 @@ public class ChatRoomService {
 		 4. Chat Room 유효성 검사
 		 5. 이미 Chat Room Member 에 존재
 		*/
-		MemberEntity memberEntity = memberRepository.findByMemberSeq(enterChatRoomRequestDto.memberSeq());
-		scheduleRepository.findByScheduleSeq(enterChatRoomRequestDto.scheduleSeq());
-		scheduleMemberRepository.findAcceptedScheduleMemberInSchedule(
-			enterChatRoomRequestDto.scheduleSeq(),
-			enterChatRoomRequestDto.memberSeq()
-		).getSchedule();
-		ChatRoomEntity chatRoomEntity = chatRoomRepository.findByChatRoomSeq(enterChatRoomRequestDto.chatRoomSeq());
+		MemberEntity memberEntity = memberRepository.findByMemberSeq(requestDto.memberSeq());
+		scheduleRepository.findByScheduleSeq(requestDto.scheduleSeq());
+		scheduleMemberRepository.findAcceptedScheduleMemberInSchedule(requestDto.scheduleSeq(), requestDto.memberSeq());
+		ChatRoomEntity chatRoomEntity = chatRoomRepository.findByChatRoomSeq(requestDto.chatRoomSeq());
 		chatRoomMemberRepository.existsChatRoomMemberEntity(memberEntity, chatRoomEntity);
 
 		// Chat Room Member Entity 생성 및 저장
@@ -83,22 +57,18 @@ public class ChatRoomService {
 	}
 
 	@Transactional
-	public void createChat(SendChatRequestDto sendChatRequestDto) {
+	public void createChat(SendChatRequestDto requestDto) {
 		/*
 		 1. Chat Room 유효성 검사
 		 2. Member 유효성 검사
 		 3. Chat Room 안에 Member 존재 유효성 검사
 		*/
-		ChatRoomEntity chatRoomEntity = chatRoomRepository.findByChatRoomSeq(sendChatRequestDto.chatRoomSeq());
-		MemberEntity memberEntity = memberRepository.findByMemberSeq(sendChatRequestDto.senderMemberSeq());
+		ChatRoomEntity chatRoomEntity = chatRoomRepository.findByChatRoomSeq(requestDto.chatRoomSeq());
+		MemberEntity memberEntity = memberRepository.findByMemberSeq(requestDto.senderMemberSeq());
 		chatRoomMemberRepository.findByMemberEntityAndChatRoomEntity(memberEntity, chatRoomEntity);
 
 		// Chat Entity 생성 및 저장
-		ChatEntity chatEntity = chatMapper.toChatEntity(
-			chatRoomEntity,
-			memberEntity,
-			sendChatRequestDto.message()
-		);
+		ChatEntity chatEntity = chatMapper.toChatEntity(chatRoomEntity, memberEntity, requestDto.message());
 
 		chatRepository.saveChatEntity(chatEntity);
 	}
