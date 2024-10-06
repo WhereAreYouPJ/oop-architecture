3ab6c8a085c44b1a04e8d528a201a8c9228f18fd
euics <pjh575157@gmail.com>
2024-10-07 01:08:07 +0900

    refactor: LocationResponseVo에서 Swagger 설명 어노테이션 제거 (#173)
    - `AddLocationResponse`와 `GetLocationResponse`의 필드에서 Swagger 어노테이션 제거
    - 코드 간결성을 위해 불필요한 어노테이션을 제거하고 유지보수성을 향상시킴

--- presentation/src/main/java/way/presentation/location/vo/res/LocationResponseVo.java
+++ presentation/src/main/java/way/presentation/location/vo/res/LocationResponseVo.java
@@ -1,23 +1,15 @@
 package way.presentation.location.vo.res;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-
 public class LocationResponseVo {
 	public record AddLocationResponse(
-		@Schema(description = "Location DB 저장 Seq")
 		Long locationSeq
 	) {
 
 	}
 
 	public record GetLocationResponse(
-		@Schema(description = "Location DB 저장 Seq")
 		Long locationSeq,
-
-		@Schema(description = "장소 이름")
 		String location,
-
-		@Schema(description = "도로명 주소")
 		String streetName
 	) {
 
