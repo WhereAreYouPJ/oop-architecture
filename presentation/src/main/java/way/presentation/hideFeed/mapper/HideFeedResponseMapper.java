9273b9e3332ce215910e40c065e408843ea7d25f
euics <pjh575157@gmail.com>
2024-10-11 01:58:32 +0900

    refactor: AddHideFeedResponse 매핑 로직 제거 (#179)
    - AddHideFeedResponse 매핑 메서드 삭제로 코드 간소화

--- presentation/src/main/java/way/presentation/hideFeed/mapper/HideFeedResponseMapper.java
+++ presentation/src/main/java/way/presentation/hideFeed/mapper/HideFeedResponseMapper.java
@@ -1,13 +1,10 @@
 package way.presentation.hideFeed.mapper;
 
-import static way.application.service.hideFeed.dto.response.HideFeedResponseDto.*;
-import static way.presentation.hideFeed.vo.response.HideFeedResponseVo.*;
-
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface HideFeedResponseMapper {
-	AddHideFeedResponse toAddHideFeedResponse(AddHideFeedResponseDto responseDto);
+
 }
