cff278f0ee68b333d9044665f4434fcba09ebc27
euics <pjh575157@gmail.com>
2024-11-11 20:29:01 +0900

    hotfix: 서버 시간 동기화

--- presentation/src/main/java/way/presentation/BackendApplication.java
+++ presentation/src/main/java/way/presentation/BackendApplication.java
@@ -1,10 +1,14 @@
 package way.presentation;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @EntityScan(basePackages = "way.application.infrastructure")
@@ -23,4 +27,8 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 }
