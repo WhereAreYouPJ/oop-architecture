c26ed746f6022afbd46eea112234a23f3239b6e9
euics <pjh575157@gmail.com>
2024-11-20 16:06:49 +0900

    feat: 회원 저장 시 초기 프로필 이미지 URL 상수 추가 (#198)
    - 회원 저장 로직에서 사용할 초기 프로필 이미지 URL을 `MemberUtil` 클래스에 상수로 추가

---
+++ service/src/main/java/way/application/service/member/util/MemberUtil.java
@@ -0,0 +1,5 @@
+package way.application.service.member.util;
+
+public class MemberUtil {
+	public static final String initialMemberProfileImage = "https://way-s3.s3.ap-northeast-2.amazonaws.com/fb7d619a-686c-4bcd-9342-e20f998e0e69.jpg";
+}
