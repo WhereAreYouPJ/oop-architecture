bd7032b392e9b0ab4b58987b4781cf87381845db
lynjun <lynjun97@naver.com>
2025-04-07 19:49:14 +0900

    로그페이지 성공 케이스 추가

--- core/src/main/java/way/application/core/configuration/WebConfig.java
+++ core/src/main/java/way/application/core/configuration/WebConfig.java
@@ -1,11 +1,18 @@
 package way.application.core.configuration;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import way.application.utils.log.aop.RequestLoggingInterceptor;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+	private final RequestLoggingInterceptor requestLoggingInterceptor;
+
 	private static final String CORS_URL_PATTERN = "/*";
 	private static final String CORS_URL = "*";
 	private static final String CORS_METHOD = "*";
@@ -13,7 +20,13 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping(CORS_URL_PATTERN)
-			.allowedOrigins(CORS_URL)
-			.allowedMethods(CORS_METHOD);
+				.allowedOrigins(CORS_URL)
+				.allowedMethods(CORS_METHOD);
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(requestLoggingInterceptor)
+				.addPathPatterns("/**"); // 전역에 적용
 	}
 }
---
+++ utils/src/main/java/way/application/utils/log/aop/RequestLoggingInterceptor.java
@@ -0,0 +1,61 @@
+package way.application.utils.log.aop;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import way.application.utils.log.entity.LogEntity;
+import way.application.utils.log.repository.LogJpaRepository;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class RequestLoggingInterceptor implements HandlerInterceptor {
+    private final LogJpaRepository requestLogRepository;
+
+    private final List<String> excludeUrls = List.of(
+            "/v3/api-docs", "/v3/api-docs/swagger-config",
+            "/swagger-ui/index.html", "/swagger-ui/swagger-ui-standalone-preset.js",
+            "/swagger-ui/index.css", "/swagger-ui/swagger-initializer.js",
+            "/swagger-ui/swagger-ui-bundle.js", "/swagger-ui/swagger-ui.css",
+            "/admin/logs"
+    );
+
+
+    private static final String START_TIME = "startTime";
+    private static final String REQUEST_BODY = "requestBody";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        request.setAttribute(START_TIME, System.currentTimeMillis());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
+        long startTime = (long) request.getAttribute(START_TIME);
+        String duration = System.currentTimeMillis() - startTime + "ms";
+
+        String url = request.getRequestURI();
+        int status = response.getStatus();
+
+        if(!excludeUrls.contains(url)) {
+            LogEntity logEntity = LogEntity.builder()
+                    .httpStatus(status)
+                    .requestUri(url)
+                    .message(duration)
+                    .errorCode("SUCCESS")
+                    .exception("SUCCESS")
+                    .timestamp(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+                    .build();
+
+            requestLogRepository.save(logEntity);
+        }
+    }
+}
+
