8f44dae5dd0eae992a97fd5297b6ff4f8c8908d9
euics <pjh575157@gmail.com>
2025-06-04 00:28:56 +0900

    feat: 회원가입 시 Initial Profile Image 수정

--- service/src/main/java/way/application/service/member/util/MemberUtil.java
+++ service/src/main/java/way/application/service/member/util/MemberUtil.java
@@ -1,5 +1,5 @@
 package way.application.service.member.util;
 
 public class MemberUtil {
-	public static final String initialMemberProfileImage = "https://way-s3.s3.ap-northeast-2.amazonaws.com/8f27555e-1602-4c03-9f1e-7788a7599855.jpg";
+	public static final String initialMemberProfileImage = "https://way-s3.s3.ap-northeast-2.amazonaws.com/b63e504d-f266-47eb-98f9-9fe05e87947c.jpg";
 }
